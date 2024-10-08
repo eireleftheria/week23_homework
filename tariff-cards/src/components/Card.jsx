15f0719e4fbec67f58d81c843f16a4e95217af76
Irina-eleftheria <ikozlova_work@inbox.ru>
2024-10-08 19:07:59 +0300

    add transform/transition

--- tariff-cards/src/components/Card.jsx
+++ tariff-cards/src/components/Card.jsx
@@ -4,15 +4,16 @@ import "../components/card.css";
 const Card = ({ card, color }) => {
   const { title, cost, speed, info } = card;
   const [isSelected, setIsSelected] = useState(false);
-  const [value, setValue] = useState(title);
-
-  function getValue(event) {
-    const newValue = event.target.value;
-    setValue(newValue);
-  }
 
   return (
-    <div className={`card ${color}`}>
+    <div
+      style={{
+        transform: isSelected ? "scale(1.1)" : "scale(1)",
+        transition: "transform 0.3s ease-in-out",
+      }}
+      onClick={() => setIsSelected(!isSelected)}
+      className={`card ${color}`}
+    >
       <div className="title">{title}</div>
       <div className="cost">{cost}</div>
       <div className="speed">{speed}</div>
